d5033c014277253995044c070905eeea9fa4d4ec
Jirka Chadima <chadima.jiri@gmail.com>
2022-10-02 10:50:29 +0200

    Fresh data, removes the necessity to convert xls to csv

--- scripts/refresh-data.sh
+++ scripts/refresh-data.sh
@@ -5,14 +5,13 @@ node prepare-gps-data.js
 PSCSOURCE='https://www.ceskaposta.cz/documents/10180/3738087/xls_pcobc.zip/50617e56-6e9a-4335-9608-96fec214e6ef'
 OKRESFILE="../data/zip/county-region.csv"
 ZIPFILE="src.zip"
-FILE='xls_pcobc.dbf'
+FILE='xls_zv_pcobc'
 PSCFILE="../data/zip/zip-county.csv"
 RESULT="../data/zip/zip-region.csv"
 
 wget "$PSCSOURCE" -O src.zip
 
 unzip -n "$ZIPFILE"
-libreoffice --headless --convert-to csv --infilter=csv:44,34,76 "$FILE.xls" --outdir . > /dev/null
 cut -d, -f2,5 "$FILE.csv" | tail -n +2 | sort | uniq > "$PSCFILE"
 join -t ',' -1 2 -2 1 -o 1.1,2.2 <(sort -t ',' -k2,2 "$PSCFILE") <(sort -t ',' -k1,1 "$OKRESFILE") | sort | uniq > "$RESULT"
 rm "$ZIPFILE"
